1500271bf70f9b84a4b1c4cf154f543637a6388d
MUHAMMAD HASSAAN TAHIR <hassaantahir991@gmail.com>
2024-12-01 16:53:34 +0500

    Create arthematic_operators.dart

---
+++ arthematic_operators.dart
@@ -0,0 +1,28 @@
+void main() {
+  // Now I will declare and intialize the variables
+
+  int a, b, c, x, y;
+
+  a = 10;
+  b = 5;
+
+  // Now I will add two numbers
+
+  c = a + b;
+  print(c);
+
+  // Now I will subtract two numbers
+
+  x = a - b;
+  print(x);
+
+  // Now I will multiply two numbers
+
+  y = a * b;
+  print(y);
+
+  // Now I will divide two numbers
+
+  double z = a / b;
+  print(z);
+}
